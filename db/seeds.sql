1bfa4035627809f16c5c7160fc7bbe9005fbf634
adam fader <adamfader@gmail.com>
2016-08-04 16:04:09 -0400

    added mysql connection and started orm.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('Angry Whopper', false, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Triple Bacon Cheeseburger', false, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Junior Bacon Cheeseburger', false, now());
