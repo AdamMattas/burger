c476496c3d81b651c547ee028f67fccc2ef390cf
adam fader <adamfader@gmail.com>
2016-08-07 14:46:31 -0400

    added burger and chef animation

---
+++ public/assets/script/script.js
@@ -0,0 +1,8 @@
+$(document).on('ready', function(){
+
+  window.onload = function burgerTime(){
+    $('.chef').animate({left: '90%'}, 1000);
+    $('.burger-top').delay( 500 ).animate({top: '460px'}, 500);
+  };
+
+});
