dddf51e6448874f24bb0d1600cf424ca23f39ce8
adam fader <adamfader@gmail.com>
2016-08-07 13:16:15 -0400

    added some styling and burger pics

--- config/orm.js
+++ config/orm.js
@@ -69,16 +69,16 @@ var orm = {
       cb(result);
     });
   },
-  delete: function (table, condition, cb) {
-    var queryString = 'DELETE FROM ' + table;
-    queryString = queryString + ' WHERE ';
-    queryString = queryString + condition;
+  // delete: function (table, condition, cb) {
+  //   var queryString = 'DELETE FROM ' + table;
+  //   queryString = queryString + ' WHERE ';
+  //   queryString = queryString + condition;
 
-    connection.query(queryString, function (err, result) {
-      if (err) throw err;
-      cb(result);
-    });
-  }
+  //   connection.query(queryString, function (err, result) {
+  //     if (err) throw err;
+  //     cb(result);
+  //   });
+  // }
 };
 
 module.exports = orm;
