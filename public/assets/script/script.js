eed6388c12bffe11bdcbf65c096d932cb330899f
adam fader <adamfader@gmail.com>
2016-08-07 18:59:22 -0400

    completed styling

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -1,8 +1,9 @@
 $(document).on('ready', function(){
 
   window.onload = function burgerTime(){
-    $('.chef').animate({left: '90%'}, 1000);
+    $('.chef').animate({left: '85%'}, 1000);
     $('.burger-top').delay( 500 ).animate({top: '460px'}, 500);
+    $('.title').animate({left: '10%'}, 1000);
   };
 
 });
