75669f38a40e70eeaacf89c6b155e8e5dedc981e
adam fader <adamfader@gmail.com>
2016-11-22 23:49:46 -0500

    commiting before deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ Here is where you make the connection to the database and export and used by the
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: 'sector001',
-  database: 'burgers_db'
+  host: 'sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'nsdvyit65z0j8uef',
+  password: 'k120xxbbxjiinqm1',
+  database: 'r1cn0t1s01sbjj52'
 });
 
 connection.connect(function (err) {
